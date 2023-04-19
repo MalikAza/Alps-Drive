5961927bf99b66bc25cb7a5524cf6c7901aaae1a
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-04-19 15:06:14 +0200

    Added get drive/{name} & some funcs improvments

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express')
-const { getDriveInfos } = require('./utils/funcs')
+const { getFolderInfos, getItemSubFolderInfos } = require('./utils/funcs')
 
 const app = express()
 app.use(express.static('frontend'))
 
 app.get('/api/drive', async (request, result) => {
-  const infos = await getDriveInfos()
+  const driveInfos = await getFolderInfos(`${__dirname}/drive`)
 
-  result.status(200).json(infos)
+  result.status(200).json(driveInfos)
 })
+
+app.get('/api/drive/:name', async (request, result) => {
+  const name = request.params.name
+  const nameInfos = await getItemSubFolderInfos(`${__dirname}/drive/${name}`)
+
+  result.status(200).json(nameInfos)
+})
+
+// app.post('/api/stuff', (req, res, next) => {
+//   console.log(req.body);
+//   res.status(201).json({
+//     message: 'Objet créé !'
+//   });
+// });
 
 module.exports = app
--- utils/funcs.js
+++ utils/funcs.js
@@ -2,35 +2,34 @@ const fs = require('fs')
 
 async function _getItemInfos(path, item) {
     // Defaults (folder) atrbs
-    let infos = {
+    let itemInfos = {
         name: item.name,
         isFolder: true
     }
     // File atrbs
     if (!item.isDirectory()) {
-        infos.isFolder = false
-        infos.size = (await fs.promises.stat(path + item.name)).size
+        itemInfos.isFolder = false
+        itemInfos.size = (await fs.promises.stat(`${path}/${item.name}`)).size
     }
     
-    return infos
+    return itemInfos
 }
 
-/**
- * This function reads the contents of a directory and returns an array of information about each item
- * in the directory.
- * @returns The `getDriveInfos` function is returning an array of objects containing information about
- * the files and directories in the `./drive` directory. The information is obtained by calling the
- * `_getItemInfos` function on each file or directory in the `./drive` directory using `Promise.all` to
- * execute the calls concurrently.
- */
-async function getDriveInfos() {
-    let infos = []
-    const items = await fs.promises.readdir('./drive', {withFileTypes: true})
+async function getFolderInfos(folderPath) {
+    let folderInfos = []
+
+    const items = await fs.promises.readdir(`${folderPath}`, {withFileTypes: true})
     await Promise.all(items.map(async (fd) => {
-        return infos.push(await _getItemInfos(`${__dirname}/drive/`, fd))
+        return folderInfos.push(await _getItemInfos(`${folderPath}`, fd))
     }))
 
-    return infos
+    return folderInfos
 }
 
-module.exports = { getDriveInfos }
+async function getItemSubFolderInfos(itemPath) {
+    const item = fs.lstatSync(itemPath)
+    if (!item.isDirectory()) return fs.readFileSync(itemPath, 'utf-8')
+    else return await getFolderInfos(itemPath)
+}
+
+module.exports = { getFolderInfos, getItemSubFolderInfos }
