a7eecdc91d81f4d98d20c8db25cd8d872f23ed81
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-04-20 13:32:12 +0200

    Added post /drive?name route

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const utilsFuncs = require('./utils/funcs')
 const os = require('os')
+const fs = require('fs')
 
 const pathDoesntExistsJson = { "message": "This path does not exists." }
 const internalServerErrorJson = { "message": "Internal Server Error" }
+const alphaNumericRegEx = new RegExp('^[a-zA-Z]+$')
 
 const app = express()
 app.use(express.static('frontend'))
@@ -21,6 +23,25 @@ app.get('/api/drive', async (request, response) => {
     else response.status(500).json(internalServerErrorJson)
   }
   
+})
+
+app.post('/api/drive', (request, response) => {
+  const name = request.query.name
+  const folderPath = `${os.tmpdir()}/${name}`
+
+  // Folder's name not alpha-numeric
+  if (!alphaNumericRegEx.test(name)) return response.status(400).send(
+    "The folder's name is not valid. It must be alpha-numeric."
+  )
+  // Folder already exists
+  if (fs.existsSync(folderPath)) return response.status(400).send(
+    "This folder already exists."
+  )
+  
+  fs.mkdirSync(folderPath)
+  response.status(201).json({
+    "message": "Folder perfetcly created."
+  })
 })
 
 app.get('/api/drive/:name', async (request, response) => {
