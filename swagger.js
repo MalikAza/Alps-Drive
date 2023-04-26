2677b5f325ed49ba482081f74039d5f32596513f
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-04-26 11:24:32 +0200

    Added swagger autogen

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutoGen = require('swagger-autogen')()
+
+const doc = {
+  info: {
+    title: 'Alps-Drive',
+    description: ''
+  },
+  host:'localhost:3000',
+  schemes: ['http']
+}
+
+const outputFile = './swagger-output.json'
+const endpointsFiles = ['./app.js']
+
+swaggerAutoGen(outputFile, endpointsFiles, doc)
